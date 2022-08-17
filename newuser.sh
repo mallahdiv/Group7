c87eb42496242d5e7b1d218938f5f3ef5466d93f
Mallah-Divine Mallah <67959364+mallahdiv@users.noreply.github.com>
2022-08-16 21:02:01 -0400

    The NewUser script

---
+++ newuser.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -   
+#title          :newuser.sh
+#description    :This script is to create a user profile and onboard them
+#author         :Mallah-Divine, Michael, Jamari, Colin, Major
+#date           :08162022
+#version        :1.0    
+#usage          :./newuser.sh
+#notes          :       
+#bash_version   :5.1.16(1)-release
+#============================================================================
+
+
