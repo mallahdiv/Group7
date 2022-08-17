9032c85d5de7b18f625b9f031d3b4e2d2572aba9
jamarikelly <jamari@jamari-VirtualBox>
2022-08-17 16:45:47 -0400

    email

--- newuser.sh
+++ newuser.sh
@@ -8,5 +8,10 @@
 #notes          :       
 #bash_version   :5.1.16(1)-release
 #============================================================================
+# this part of the script will then notify us that a new user has been
+# created by sending an email - jamari
+
+echo "$name, is a new user, profile successfully created" | mail -s "this is the subject" jamarikelly2816@gmail.com
+
 
 
