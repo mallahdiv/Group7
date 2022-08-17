b32b7d1bc4ec8e1bd0b64097133f846037ae7c64
mallahdiv <mallahdiv@gmail.com>
2022-08-17 05:26:12 -0400

    Update

--- newuser.sh
+++ newuser.sh
@@ -1,11 +1,11 @@
-#!/bin/bash -   
+#!/bin/bash -
 #title          :newuser.sh
 #description    :This script is to create a user profile and onboard them
 #author         :Mallah-Divine, Michael, Jamari, Colin, Major
 #date           :08162022
-#version        :1.0    
+#version        :1.0
 #usage          :./newuser.sh
-#notes          :       
+#notes          : 
 #bash_version   :5.1.16(1)-release
 #============================================================================
 
